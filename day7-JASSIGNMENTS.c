2454cd3fd792e3167a817ae413a147b7253cb70d
Shishupal855 <shiva.chauhan0718@gmail.com>
2024-10-04 19:37:05 +0530

    Add files via upload

---
+++ day7-JASSIGNMENTS.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+	int main(void) 
+{
+	int T;
+	scanf("%d",&T);
+	while(T--)
+	{
+	    int x;
+	    scanf("%d",&x);
+	    if(x<=7)
+	    {
+	        printf("\nyes");
+	    }
+	    else
+	    {
+	        printf("\nno");
+	    }
+	}
+	return 0;
+}
+
+
